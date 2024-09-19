7a966194fd8b22b11005affa036256a51c71797c
mu4a_ <mu4a_@DESKTOP-4DP2NVH>
2024-09-19 09:37:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int int1 = 23;
+    int int2 = -52;
+    int int3 = 79;
+    int int4 = 3;
+
+    cout << "int1 " << int1 << "int2 " << int2 << endl;
+    while (int1 != int2) {
+        int1++;
+
+    }
+    cout << "int1 " << int1 << "int2 " << int2 << endl;
+    int int5 = 38;
+
+    int int6 = 13;
+    cout << "int5  " << int5 << "int6  " << int6 << endl;
+
+    while (int5 > int6) {
+        int6++;
+    }
+    cout << "int5 " << int5 << "int6 " << int6 << endl;
+}
